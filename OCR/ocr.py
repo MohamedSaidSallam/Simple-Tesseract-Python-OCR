c6294f3f5c5cd04049ef18d3d2037c0f229d923b
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-01-03 14:42:56 +0200

    feat: preprocessing

--- OCR/ocr.py
+++ OCR/ocr.py
@@ -1,19 +1,49 @@
 import cv2
 import pytesseract
 
+import ocr.utility as utility
 
 TESSERACT_CONFIG = "-l eng --oem 1 --psm 6"
 
 OUTPUT_PATH = 'output/'
 
 
+def preprocessImage(image):
+    grayscale = utility.getGrayScaleImage(image)
+    cv2.imshow("grayscale", grayscale)
+    noNoise = utility.removeNoise(grayscale)
+    cv2.imshow("noNoise", noNoise)
+    threshold = utility.applyThresholding(noNoise)
+    cv2.imshow("threshold", threshold)
+    eroded = utility.getErodedImage(threshold)
+    cv2.imshow("eroded", eroded)
+    return eroded
+
+
 def main(imgPath, outputFileName):
 
     image = cv2.imread(imgPath)
 
-    output = pytesseract.image_to_string(image, config=TESSERACT_CONFIG)
+    cv2.imshow("The Image Loaded", image)
 
-    output = "".join([c if ord(c) < 128 else "" for c in output]).strip()
+    preprocessed = preprocessImage(image)
+
+    contours, _ = cv2.findContours(preprocessed, cv2.RETR_EXTERNAL,
+                                   cv2.CHAIN_APPROX_NONE)
+    output = []
+
+    for contour in contours:
+        x, y, width, height = cv2.boundingRect(contour)
+
+        text = pytesseract.image_to_string(
+            preprocessed[y:y + height, x:x + width], config=TESSERACT_CONFIG)
+
+        text = "".join(
+            [char if ord(char) < 128 else "" for char in text]).strip()
+
+        output.append(text)
 
     with open(OUTPUT_PATH + outputFileName, 'w') as outputFile:
-        outputFile.write(output)
+        outputFile.write('\n'.join(output))
+
+    cv2.waitKey(0)
---
+++ OCR/utility.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+DEFAULT_MEDIAN_BLUR_KSIZE = 3
+DEFAULT_CANNY_THRESHIKD_1 = 100
+DEFAULT_CANNY_THRESHIKD_2 = 200
+DEFAULT_DILATE_KERNAL = np.ones((3, 3), np.uint8)
+DEFAULT_ERODE_KERNAL = np.ones((3, 3), np.uint8)
+DEFAULT_OPEN_KERNAL = np.ones((5, 5), np.uint8)
+DEFAULT_CLOSE_KERNAL = np.ones((5, 5), np.uint8)
+
+
+def getGrayScaleImage(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+def removeNoise(image):
+    return cv2.medianBlur(image, DEFAULT_MEDIAN_BLUR_KSIZE)
+
+
+def applyThresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY)[1]
+
+
+def getDilatedImage(image):
+    return cv2.dilate(image, DEFAULT_DILATE_KERNAL, iterations=1)
+
+
+def getErodedImage(image):
+    return cv2.erode(image, DEFAULT_ERODE_KERNAL, iterations=1)
+
+
+def applyOpening(image):
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, DEFAULT_OPEN_KERNAL)
+
+
+def applyClosing(image):
+    return cv2.morphologyEx(image, cv2.MORPH_CLOSE, DEFAULT_CLOSE_KERNAL)
+
+
+def getCannyResult(image):
+    return cv2.Canny(image, DEFAULT_CANNY_THRESHIKD_1, DEFAULT_CANNY_THRESHIKD_2)
